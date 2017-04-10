5f0dc80d22d974d5183cafb4a0fdea22b2808997
EmixMaxime <emixmaxime@outlook.fr>
2017-04-10 21:59:44 +0200

    Fix setCookie without bag

--- src/response/cookies.js
+++ src/response/cookies.js
@@ -4,22 +4,24 @@
  * 
  */
 
-const setCookie = (
+const setCookieBag = (
   { maxAge = 1 * 60 * 60 * 1000, secure = true, httpOnly = true }
 ) => (response, cookieName, data) => response.cookie(cookieName, data, { maxAge, secure, httpOnly });
+
+const setCookie = (response, cookieName, data, opts = {}) => setCookieBag(opts)(response, cookieName, data);
 
 /**
  * Usage : setCookie({secure: true, cookieName: 'XSRF-TOKEN'})(expressResponse, 'hello world');
  *
  * @param {Object} options 
  */
-const createCookieBag = function ({ setCookie }, opts = {}) {
-  const setCookiee = setCookie(opts); // "Instanciate"
+const createCookieBag = function ({ setCookieBag }, opts = {}) {
+  const setCookie = setCookieBag(opts); // "Instanciate"
 
   const { cookieName } = opts;
   if (!cookieName) throw new Error("You can't create a setCookieBag without opts.cookieName");
 
-  return (res) => ({ setCookie: setCookiee.bind(null, res, cookieName) });
+  return (res) => ({ setCookie: setCookie.bind(null, res, cookieName) });
 };
 
 const createCookieBagFactory = deps => createCookieBag.bind(null, deps);
@@ -28,27 +30,27 @@ const createCookieBagFactory = deps => createCookieBag.bind(null, deps);
 // Ensuite c'est accessible depuis cookies.nameBag.bagFunction
 
 /** Exemples to deal with csrf and jwt cookies in my applications */
-const setSessionCookie = setCookie({ name: 'XSRF-TOKEN' });
+const setSessionCookie = setCookieBag({ name: 'XSRF-TOKEN' });
 
-const setJwtCookie = setCookie({
+const setJwtCookie = setCookieBag({
   secure: true,
   httpOnly: true,
   cookieName: 'JWT-TOKEN',
   maxAge: 1 * 60 * 60 * 1000,
 });
 
-const cookies = ({ setCookie }, options) => {
-  const setCookiee = setCookie(options); // "Instanciate"
+const cookies = ({ setCookie }, options = {}) => {
 
   return (res) => ({
-    setCookie: setCookiee.bind(null, res),
+    setCookie: setCookie.bind(null, res),
   });
+
 };
 
 const cookiesFactory = deps => cookies.bind(null, deps);
 
 module.exports = {
   cookies: cookiesFactory({ setCookie }),
-  createCookieBag: createCookieBagFactory({ setCookie }),
+  createCookieBag: createCookieBagFactory({ setCookieBag }),
   setCookie,
 };
