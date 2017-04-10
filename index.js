9506e428468bebfa923568d97400c2ca964af8b7
EmixMaxime <emixmaxime@outlook.fr>
2017-04-10 14:34:10 +0200

    add index.js (entrypoint)

---
+++ index.js
@@ -0,0 +1,6 @@
+const response = require('../src/response/ResponseServiceProvider');
+const request = require('../src/request/RequestServiceProvider');
+
+module.exports = {
+  response, request,
+};
