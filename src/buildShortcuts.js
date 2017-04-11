85d8195eaa4e72aa586506d6c06e96e8443d8376
EmixMaxime <emixmaxime@outlook.fr>
2017-04-11 11:09:15 +0200

    Fix buildShortcuts

--- src/buildShortcuts.js
+++ src/buildShortcuts.js
@@ -4,12 +4,12 @@ const buildShortcuts = (req, bindOptions = {}, boundableFunction = []) => {
   if (bindOptions) {
     Object.keys(bindOptions).forEach(functionName => {
       const opts = bindOptions[functionName];
+      
       opts.forEach(opt => {
-        const f = boundableFunction.map(f => f.name === functionName ? f : false);
+        const f = boundableFunction.filter(f => f.name === functionName ? f : false);
         const referenceFunction = f[0];
 
         if (f && typeof referenceFunction === 'function') {
-          console.log({opts});
           shortcuts[opt.name] = referenceFunction.bind(null, req, opt.value);
         }
       });
